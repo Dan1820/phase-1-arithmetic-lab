285e4cff6f3df0a11e4376937a70072ebe1a16d5
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-03-14 16:05:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let num1=31;
+let num2=2;
+const multiply=num1*num2;
+
+
+const random= parseInt (Math.random()*10)+1
+console.log(random)
+
+let num3=7;
+let num4=18;
+const mod= num4%num3;
+console.log(mod);
+const set1=[1,3,4,20,2]
+const max=Math.max(...set1)
+console.log(max)
